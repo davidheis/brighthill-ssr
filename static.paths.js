30e864e1ee83608a2108ddb9537dcf77820d4e4e
david heisner <davidheis@hotmail.com>
2019-03-19 14:12:18 +1000

    add favicons, cors.json, auth id to correspond to this firebase database

--- static.paths.js
+++ static.paths.js
@@ -1,4 +1,5 @@
 export default [
     '/',
-    'https://brighthilllawyers.com/articles/Residential-and-Commercial-Property-Retainer-Wall-Liability/show/-LFzsixMRicQOhNkZEhw'
+    'https://brighthilllawyers.com/articles/Residential-and-Commercial-Property-Retainer-Wall-Liability/show/-LFzsixMRicQOhNkZEhw',
+    'https://brighthill-ssr.firebaseapp.com/articles/Residential-and-Commercial-Property-Retainer-Wall-Liability/show/-LFzsixMRicQOhNkZEhw'
 ];
